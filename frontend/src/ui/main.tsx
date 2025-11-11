21b823439c8eb6d73a7a1bcc701b5e0b5a4dfd16
Ayush Petwal <ayushpetwal.0105@gmail.com>
2025-11-11 15:59:13 +0530

    fix: minor environment injection

--- frontend/src/ui/main.tsx
+++ frontend/src/ui/main.tsx
@@ -6,29 +6,19 @@ import { HashRouter } from 'react-router-dom';
 import { SettingsProvider } from './contexts/SettingsContext.tsx';
 import { TrainingProvider } from './contexts/TrainingContext.tsx';
 import { WalletConnectProvider } from './contexts/WalletConnectContext.tsx';
-// import { WalletConnectClient } from './services/walletConnectClient.ts';
+import { WalletConnectClient } from './services/walletConnectClient.ts'; 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <SettingsProvider>
-      <WalletConnectProvider>
-        <HashRouter>
-          <TrainingProvider>
-          <App />
-          </TrainingProvider>
-        </HashRouter>
-      </WalletConnectProvider>
+      <TrainingProvider>
+        <WalletConnectProvider>
+          <WalletConnectClient />
+            <HashRouter> 
+              <App />
+            </HashRouter>
+          </WalletConnectProvider>
+        </TrainingProvider>
     </SettingsProvider>
   </StrictMode>
 );
-
-
-
-//       {/* <TrainingProvider>
-//         <WalletConnectProvider>
-//           <WalletConnectClient />
-//           <HashRouter> */}
-
-//           {/* </HashRouter>
-//         </WalletConnectProvider>
-//       </TrainingProvider>
